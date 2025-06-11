9a17d126ebc3f3d59280c36c632bf38b298c8ac4
breezynotcheezy <thebannanashotz@gmail.com>
2025-06-11 11:37:03 -0400

    bld

--- filter/exceptions.py
+++
@@ -1,6 +0,0 @@
-class FastbackError(Exception):
-    """Operation error"""
-class UnsupportedType(FastbackError):
-    """specified engine unavailable"""
-class EngineFailure(FastbackError):
-    """Engine integration exception"""
